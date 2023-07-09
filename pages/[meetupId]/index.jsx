35f8a4bdd4d44414ecdd274ed12c7265cad1cd2b
itxSaaad <saadstudent.cs@gmail.com>
2023-07-09 14:24:09 +0500

    Added: GetStaticProps on NewMeetupPage

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,14 +1,51 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetailPage() {
+function MeetupDetailPage(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="A First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  // fetch data for a single meetup
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
 }
 
 export default MeetupDetailPage;
